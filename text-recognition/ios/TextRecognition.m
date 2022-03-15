5116705bed8f1e86843db7ab7381c38fc3f6c0b9
wego <wego-deluxe@gmx.de>
2022-03-15 12:55:07 +0100

    some code beautifying for easy debugging

--- text-recognition/ios/TextRecognition.m
+++ text-recognition/ios/TextRecognition.m
@@ -26,11 +26,11 @@ RCT_EXPORT_MODULE()
 
 - (NSDictionary*)lineToDict: (MLKTextLine*)line {
     NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-    
+
     [dict setObject:line.text forKey:@"text"];
     [dict setObject:[self frameToDict:line.frame] forKey:@"frame"];
     [dict setObject:[self langsToDicts:line.recognizedLanguages] forKey:@"recognizedLanguages"];
-    
+
     NSMutableArray *elements = [NSMutableArray array];
     for (MLKTextElement* element in line.elements) {
         [elements addObject:@{
@@ -39,47 +39,42 @@ RCT_EXPORT_MODULE()
         }];
     }
     [dict setObject:elements forKey:@"elements"];
-    
+
     return dict;
 }
 
 - (NSDictionary*)blockToDict: (MLKTextBlock*)block {
     NSMutableDictionary *dict = [NSMutableDictionary dictionary];
-    
+
     [dict setObject:[self frameToDict:block.frame] forKey:@"frame"];
     [dict setObject:block.text forKey:@"text"];
     [dict setObject:[self langsToDicts:block.recognizedLanguages] forKey:@"recognizedLanguages"];
-    
+
     NSMutableArray *lines = [NSMutableArray array];
     for (MLKTextLine* line in block.lines) {
         [lines addObject:[self lineToDict:line]];
     }
     [dict setObject:lines forKey:@"lines"];
-    
+
     return dict;
 }
 
-RCT_EXPORT_METHOD(recognize: (nonnull NSString*)url
-                  resolver:(RCTPromiseResolveBlock)resolve
-                  rejecter:(RCTPromiseRejectBlock)reject)
-{
+RCT_EXPORT_METHOD(recognize: (nonnull NSString*)url resolver:(RCTPromiseResolveBlock)resolve rejecter:(RCTPromiseRejectBlock)reject) {
     NSURL *_url = [NSURL URLWithString:url];
     NSData *imageData = [NSData dataWithContentsOfURL:_url];
     UIImage *image = [UIImage imageWithData:imageData];
     MLKVisionImage *visionImage = [[MLKVisionImage alloc] initWithImage:image];
     MLKTextRecognizer *textRecognizer = [MLKTextRecognizer textRecognizer];
-    
-    [textRecognizer processImage:visionImage
-                      completion:^(MLKText *_Nullable _result,
-                                   NSError *_Nullable error) {
+
+    [textRecognizer processImage:visionImage completion:^(MLKText *_Nullable _result, NSError *_Nullable error) {
         if (error != nil || _result == nil) {
             return reject(@"Text Recognition", @"Text recognition failed", error);
         }
-        
+
         NSMutableDictionary *result = [NSMutableDictionary dictionary];
-        
+
         [result setObject:_result.text forKey:@"text"];
-        
+
         NSMutableArray *blocks = [NSMutableArray array];
         for (MLKTextBlock *block in _result.blocks) {
             [blocks addObject:[self blockToDict:block]];
